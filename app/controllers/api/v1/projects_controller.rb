3cf6c04379bbbe3132290fd471dcc9907c2f0489
Theo Kang <theokang@Theos-iMac.local>
2023-10-25 10:04:51 +1030

    add httparty and parse json data

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,6 +1,39 @@
+require 'json'
+
 class Api::V1::ProjectsController < ApplicationController
   def index
-    render json: {"id": "633"}, status: 200
+    result = '{
+  "data": {
+    "type": "projects",
+    "attributes": {
+      "name": "test name",
+      "project_type_id": 2
+    },
+    "relationships": {
+      "company": {
+        "data": {
+          "type": "companies",
+          "id": "2081"
+        }
+      },
+      "project_manager": {
+        "data": {
+          "type": "people",
+          "id": "2748"
+        }
+      },
+      "workflow": {
+        "data": {
+          "type": "workflows",
+          "id": "633"
+        }
+      }
+    }
+  }
+}'
+    json_result = JSON.parse(result)
+    puts json_result["data"]["type"]
+    render json: result, status: 200
   end
 
   def show
