4d080937b5dfecc3c0b04fd8d1c09e024ee80b58
Theo Kang <theokang@Theos-iMac.local>
2023-10-25 18:00:49 +1030

    post request to create a project

--- app/controllers/api/v1/api_test_controller.rb
+++ app/controllers/api/v1/api_test_controller.rb
@@ -1,16 +1,16 @@
 class Api::V1::ApiTestController < ApplicationController
-  include Authentication
-  include Projects
+  include ProjectsApi
 
   def test_get_project_by_id 
     # result = get_project_by_id 560
     # puts result
 
-    puts "headers: " + set_headers.to_s
+    # puts "headers: " + set_headers.to_s
 
-    @project = Projects::ProjectsGet.new()
+    @project = ProjectsApi::Projects.new()
     @project.get_projects
-    @project.get_project_by_id 566
+    # @project.get_project_by_id 566
+    @project.create_project
   end
 
 end
---
+++ app/controllers/concerns/projects_api.rb
@@ -0,0 +1,82 @@
+module ProjectsApi
+  extend ActiveSupport::Concern
+
+  # optimize: split the class into several by "GET", "POST", etc.
+  class Projects
+    include SetHeaders
+    include SetBodys
+
+    attr_accessor :api_endpoint
+    attr_accessor :data
+    attr_accessor :errors
+
+    # the caller can change the endpoint as needed when creating an instance
+    def initialize api_endpoint = "https://api.productive.io/api/v2/projects"
+      @api_endpoint = api_endpoint
+    end
+
+    def get_projects
+      response = HTTParty.get("https://api.productive.io/api/v2/projects", :headers => set_auth_headers)
+      @data = response.parsed_response["data"]
+      # puts @result
+
+      @data.each do |hash|
+        puts hash["id"] + " " + hash["type"]
+      end
+    end
+
+    def get_project_by_id id
+      url = "https://api.productive.io/api/v2/projects/#{id}"
+      # puts url
+
+      response = HTTParty.get(url, :headers => set_auth_headers)
+      @errors = response.parsed_response["errors"]
+      pp @errors
+    end
+
+    def create_project
+      options = { :headers => set_body_headers, :body => set_bodys }
+      puts options
+      response = HTTParty.post(@api_endpoint, options)
+      # should be successful, but ...
+      # @errors = response.parsed_response["errors"]
+      # pp @errors
+
+      pp response
+      @data = response.parsed_response["data"]
+      # puts @result
+      @data.each do |hash|
+        puts hash["id"] + " " + hash["type"]
+      end
+    end
+
+    def update_project
+
+    end
+
+    def archive_project
+
+    end
+
+    def restore_project
+
+    end
+
+    def delete_project
+
+    end
+
+    def copy_project
+
+    end
+
+    def change_workflow_project
+
+    end
+
+    def map_to_workflow_project
+
+    end
+
+  end
+end
