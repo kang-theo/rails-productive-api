681cfb168006d49fa6ce8bfb6c4da4af2f10b2d9
Theo Kang <theokang@Theos-iMac.local>
2023-10-25 12:00:23 +1030

    create authentication module and set_headers methodw

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,41 +1,29 @@
-require 'json'
+# require 'json'
 
 class Api::V1::ProjectsController < ApplicationController
   def index
-    result = '{
-  "data": {
-    "type": "projects",
-    "attributes": {
-      "name": "test name",
-      "project_type_id": 2
-    },
-    "relationships": {
-      "company": {
-        "data": {
-          "type": "companies",
-          "id": "2081"
-        }
-      },
-      "project_manager": {
-        "data": {
-          "type": "people",
-          "id": "2748"
-        }
-      },
-      "workflow": {
-        "data": {
-          "type": "workflows",
-          "id": "633"
-        }
-      }
-    }
-  }
-}'
-    json_result = JSON.parse(result)
-    puts json_result["data"]["type"]
-    render json: result, status: 200
+    # response = HTTParty.get("https://api.productive.io/api/v2/projects", :headers => {
+    #   "X-Organization-Id" => "26018",
+    #   "Content-Type" => "application/vnd.api+json",
+    #   "X-Auth-Token" => "bc415e7d-52e0-4532-82f5-d924c3a9afe6"
+    # }
+    response = HTTParty.get("https://api.productive.io/api/v2/projects", :headers => set_headers)
+
+    # puts result
+    # puts result["data"]
+    # json_result = JSON.parse(result.data)
+    # puts json_result
+    result = response.parsed_response["data"]
+    puts result
+
+    result.each do |hash|
+      puts hash["id"] + " " + hash["type"]
+    end
+
+    render json: {}, status: 200
   end
 
   def show
+    puts set_headers
   end
 end
