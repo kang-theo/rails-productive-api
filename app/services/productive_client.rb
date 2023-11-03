fcf370b648112d330094504eb534f856e9eceb8b
kang-theo <theo.cy.kang@gmail.com>
2023-11-03 22:46:12 +0800

    get onee and all projects as objects with attributes belong to project itself

--- app/services/productive_client.rb
+++ app/services/productive_client.rb
@@ -1,11 +1,8 @@
-# require 'active_support/core_ext/hash'
 class ProductiveClient
   include HTTParty
   base_uri 'https://api.productive.io/api/v2'
 
   def initialize()
-    # api_key = "c664831b-4419-4bb0-9dc0-09816f04cea2"
-    # org_id = "30958"
     @headers = {
       "X-Auth-Token" => Rails.application.credentials.productive_api_token,
       "X-Organization-Id" => Rails.application.credentials.organization_id.to_s,
@@ -26,11 +23,15 @@ class ProductiveClient
   def handle_response(response)
     if response.success? && !response.body.blank?
       parsed_data = JSON.parse(response.body)["data"]
-      # debugger
-      # puts parsed_data
-      r = Project.new(parsed_data)
-      puts r.to_s
-      # debugger
+      project_result = Array.new()
+
+      if parsed_data.is_a?(Array)
+        parsed_data.map {|item| project_result.push(Project.new(item))}
+      elsif
+        project_result.push(Project.new(parsed_data))
+      end
+      debugger
+      return project_result
     else
       raise "API request failed with status #{response.code}: #{response.body}"
     end
--- app/services/project.rb
+++ app/services/project.rb
@@ -6,44 +6,33 @@ class Project
 
 # testing data: {"id": 1, "type": "testc", "attributes": { "name": "test project", "number": "1", "project_number": "1", "project_type_id": "2", "project_color_id": "null", "last_activity_at": "2023-10-23T06:10:48.000+02:00", "public_access": true, "time_on_tasks": true, "tag_colors": {}, "archived_at": "null", "created_at": "2023-10-23T06:10:48.107+02:00", "template": false, "budget_closing_date": "null", "needs_invoicing": false, "custom_fields": "null", "task_custom_fields_ids": "null", "sample_data": false }}
   def initialize(data)
-    @data = data
-    @attrs_hash = Hash.new()
-    # debugger
-    # json_data = JSON.parse(ActiveSupport::JSON.encode(@data))
-    # json_data = @data
-    # debugger
-    project_attrs = ProjectAttr.new(@data["attributes"])
-    attr_keys = project_attrs.keys
-
-    attr_keys.each do |key|
-      @attrs_hash[key] = project_attrs.send(key)
-    end
-
+    @data = construct_instance_var(data)
     create_accessors
   end
 
-  # create accessors dynamically for project and its attributes
-  def create_accessors
-    if @data.blank? && @attrs_hash.blank?
-      raise "Invalid attributes." 
+  def construct_instance_var(data)
+    attr_hash = data["attributes"]
+    root_hash = Hash.new()
+
+    data.each do |key, value|
+      unless value.is_a?(Hash)
+        root_hash[key] = value
+      end
     end
 
-    create_accessors_for_hash(@attrs_hash)
-    create_accessors_for_hash(@data)
+    root_hash.merge(attr_hash)
   end
 
-  def create_accessors_for_hash(hash)
-    hash.each do |key, value|
-      if value.is_a?(Hash) 
-        return
-      end
-
+  def create_accessors
+    @data.each do |key, value|
       class_eval do
         define_method(key) do
+          # instance_variable_get("@#{key}")
           value
         end
 
         define_method("#{key}=") do |new_value|
+          # instance_variable_set("@#{key}", new_value)
           hash[key] = new_value
         end
       end
@@ -51,28 +40,10 @@ class Project
   end
 
   def to_s
-    # instance_variables.map do |var|
-    #   puts var
-    #   value = instance_variable_get(var)
-    #   "#{var}: #{value}"
-    # end
-    puts @data["id"], @data["type"]
-
-    puts "2---"
-    puts @data["attributes"]
-    puts @data["attributes"].class
-
-    @data.each do |value|
-      if value.is_a?(Hash)
-        return
-      end
-      puts "3---"
-      puts value.class
-      puts value
-    end
-
-    @attrs_hash.each do |attr|
-      puts attr
+    instance_variables.map do |var|
+      puts var
+      value = instance_variable_get(var)
+      "#{var}: #{value}"
     end
   end
 
