bb414595c76c308340f19d57c61d0d51df78c496
kang-theo <theo.cy.kang@gmail.com>
2023-11-06 14:42:27 +1030

    check the exception at the beginning, and process endpoints as a hash

--- app/services/productive_client.rb
+++ app/services/productive_client.rb
@@ -14,35 +14,35 @@ class ProductiveClient
   end
 
   def all()
-    process_request()
+    process_request({})
   end
 
   def find(id)
-    process_request(id)
+    process_request({id: id})
   end
 
   private
 
   def handle_response(response)
-    if response.success? && !response.body.blank?
-      parsed_data = JSON.parse(response.body)["data"]
-      project_result = Array.new()
-
-      if parsed_data.is_a?(Array)
-        parsed_data.map {|item| project_result.push(Project.new(item))}
-      elsif
-        project_result.push(Project.new(parsed_data))
-        debugger
-      end
-      return project_result
-    else
+    if !response.success? || response.body.blank?
       raise "API request failed with status #{response.code}: #{response.body}"
     end
+
+    parsed_data = JSON.parse(response.body)["data"]
+    project_result = Array.new()
+
+    if parsed_data.is_a?(Array)
+      parsed_data.map {|item| project_result.push(Project.new(item))}
+    elsif
+      project_result.push(Project.new(parsed_data))
+    end
+    project_result
   end
 
-  def process_request(endpoint = "")
-    logger.info("Http Request: #{self.class.default_options[:base_uri]}/#{pluralized_resource_name()}/#{endpoint}")
-    response = self.class.get("/#{pluralized_resource_name()}/#{endpoint}", headers: @headers)
+  def process_request(req_params)
+    debugger
+    logger.info("Http Request: #{self.class.default_options[:base_uri]}/#{pluralized_resource_name()}/#{req_params[:id]}")
+    response = self.class.get("/#{pluralized_resource_name()}/#{req_params[:id]}", headers: @headers)
     handle_response(response)
   end
 
