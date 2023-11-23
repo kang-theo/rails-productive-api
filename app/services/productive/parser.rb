6385f1bd0295a61d19988e738a6c6b20a2052649
kang-theo <theo.cy.kang@gmail.com>
2023-11-23 16:06:07 +1030

    parse id and type, and use type as the association method name

--- app/services/productive/parser.rb
+++ app/services/productive/parser.rb
@@ -2,49 +2,97 @@
 
 module Productive
   module Parser
-    extend self
+    # extend self
+    # module_function
 
-    attr_accessor :instance_attrs, :foreign_key_types, :instance_class
-    @instance_attrs = {}
-    @foreign_key_types = []
-    @instance_class = nil
+    # attr_accessor :instance_class
+    # @instance_attrs = {}
+    # @foreign_key_types = []
+    class << self
+      attr_accessor :instance_class
+    end
 
     def included(base)
       self.instance_class = base
     end
 
-    def handle_response(response)
+    def self.handle_response(response)
       raise ApiRequestError, "API request failed with status #{response.code}: #{response.body}" unless response.success?
-      raise ApiRequestError, "API response is blank" if response.body.blank?
       
       begin
-        parsed_data = JSON.parse(response.body)['data']
+        data_hash = JSON.parse(response.body)['data']
       rescue JSON::ParserError => e
         Rails.logger.error "JSON::ParserError: #{e.message}"
-        render json: { error: 'JSON::ParserError' }, status: :bad_data
+        render json: { error: 'JSON::ParserError' }, status: :bad_response
       end
 
-      instance_results = []
-      flatten_data = parsed_data.is_a?(Array) ? parsed_data : [parsed_data]
+      attributes = data_hash['attributes'].merge(id: data_hash['id'])
+      association_types = []
 
-      flatten_data.each do |datum|
-        parse_attributes_and_types(datum)
+      data_hash['relationships'].each do |_key, value|
+        data = value["data"]
 
-        # creating instances
-        entity = instance_class.new(instance_attrs, foreign_key_types)
-        instance_results.push(entity)
+        if data.blank?
+          # custom_field_xx, template_object
+          next
+        elsif data.is_a?(Array)
+          type = nil
+          ids = data.map do |data| 
+            type ||= data["type"]
+            data["id"] 
+          end
+
+          association_types.push(type)
+          attributes["#{type.singularize}_ids"] = ids
+        else
+          type = data['type']
+          id = data["id"]
+
+          association_types.push(type)
+          attributes["#{type.singularize}_id"] = id
+        end
       end
-      instance_results
-    end
+
+      debugger
+
+      # build_associations(ids_and_types)
+      ids_and_types.each do |item|
+        if item.is_a?(Array)
+          attributes["#{type.downcase}_ids".to_sym] = ids
+        else
+          next
+          attributes["#{item.downcase}_id".to_sym] = item
+        end
+      end
+    end 
 
     private
 
-    def parse_attributes_and_types(data)
-      self.instance_attrs.clear # TODO: no
-      self.foreign_key_types.clear
+    def self.parse_relationships(relationships)
+      relationships.map do |_key, value|
+        data = value["data"]
 
-      instance_attrs[:id] = data['id']
-      instance_attrs.merge!(data['attributes'])
+        if data.is_a?(Array)
+          data.map { |item| { type: item["type"], id: item["id"] } } 
+        elsif data.nil?
+          { type: nil, id: nil }
+        else
+          { type: data["type"], id: data["id"] }
+        end
+      end
+    end
+
+    def build_associations(ids_and_types)
+      ids_and_types.each do |item|
+        "#{item[:type]}_id"
+      end
+    end
+
+    def self.parse_attributes_and_types(data, attributes, relationship_types)
+      attributes[:id] = data['id']
+      attributes.merge!(data['attributes'])
+
+
 
       data['relationships'].each do |_key, value|
         next unless value.is_a?(Hash)
@@ -58,10 +106,10 @@ module Productive
         # eg. "memberships_id": ["6104455", "6104456", "6104457", "6104464"]
         #     "project_manager": {"data"=>{"type"=>"people", "id"=>"412034"}}
         type = flatten_data.first['type']
-        foreign_key_types.push(type)
+        relationship_types.push(type)
 
         # TODO: id should be conformed to the convention
-        instance_attrs[(type.singularize + '_id').to_sym] = flatten_data.map do |datum| # TODO: to many loop and if, need to be concise
+        attributes[(type.singularize + '_id').to_sym] = flatten_data.map do |datum| # TODO: to many loop and if, need to be concise
           next if datum.blank?
           datum['id']
         end
