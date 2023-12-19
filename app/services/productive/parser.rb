9059b26187833b2802d5b47f5067cf9fb27963ca
kang-theo <theo.cy.kang@gmail.com>
2023-12-19 14:39:46 +1030

    Refactor/ replace method with method object, referring to Refactoring Ruby Edition 6.9

--- app/services/productive/parser.rb
+++ app/services/productive/parser.rb
@@ -2,18 +2,38 @@
 
 module Productive
   module Parser
-    def self.handle_response(response, klass)
+    def self.handle_response(response, entity_class)
+      # refactor: replace method with method object
+      ResponseHandler.new(self, response, entity_class).compute
+    end
+  end
+
+  # refactor: replace method with method object
+  class ResponseHandler
+    attr_accessor :parser, :response, :entity_class, :data, :attributes, :association_info
+
+    def initialize(parser, response, entity_class)
+      @parser = parser
+      @response = response
+      @entity_class =entity_class 
+
+      @attributes = {}
+      @association_info = {}
+    end
+
+    def compute
       return [] unless response.code == 200
 
       parsed_data = response.body.dig('data')
       flatten_data = parsed_data.is_a?(Array) ? parsed_data : [parsed_data]
 
+      klass = entity_class.class != Class ? entity_class.class : entity_class 
+
       entities = flatten_data.map do |data_hash|
-        association_info = parse_associations_info(data_hash)
-        attributes = parse_attributes(data_hash, association_info)
+        parse_associations_info(data_hash)
+        parse_attributes(data_hash)
 
         # create instances
-        klass = klass.class != Class ? klass.class : klass
         klass.new(attributes, association_info)
       end
 
@@ -29,9 +49,7 @@ module Productive
     #
     # @param [Hash] data_hash: The hash containing association information.
     # @return [Hash] Parsed association information.
-    def self.parse_associations_info(data_hash)
-      association_info = {}
-
+    def parse_associations_info(data_hash)
       relationships = data_hash['relationships']
       relationships.each do |key, value|
         data = value['data']
@@ -41,10 +59,8 @@ module Productive
         # association_info[key] = flatten_data.map { |flatten_data| flatten_data["id"] }
         association_info[key] = data.is_a?(Array) ? data.map { |datum| datum['id'] } : data['id']
       end
-
-      association_info
     end
-
+    
     # Parses attributes from a hash, considering association information.
     #
     # Example Input:
@@ -54,15 +70,14 @@ module Productive
     # @param [Hash] data_hash: The hash containing attribute and relationship information.
     # @param [Hash] association_info: The hash containing association information.
     # @return [Hash] Parsed attributes.
-    def self.parse_attributes(data_hash, association_info)
-      attributes = data_hash['attributes'].merge('id' => data_hash['id'])
+    def parse_attributes(data_hash)
+      attributes.merge!(data_hash['attributes'].merge('id' => data_hash['id']))
 
       association_info.each do |key, value|
         association_key = "#{key.singularize}_id#{'s' if value.is_a?(Array)}"
         attributes.merge!(association_key => value)
       end
-
-      attributes
     end
+
   end
 end
